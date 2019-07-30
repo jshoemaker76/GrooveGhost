6da86113cbe5a69ba13d08edc39dc274c68c4d2d
John Shoemaker <jshoemaker9999@gmail.com>
2019-07-30 19:45:22 -0400

    init

---
+++ GrooveGhost/Assets/ChangeCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    public Texture2D texture;
+    void Start()
+    {
+        Vector2 cursorHotspot = new Vector2(texture.width / 2, texture.height / 2);
+        Cursor.SetCursor(texture, cursorHotspot, CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+Won best game overall at Gator Game Jam 2019.
+
+Other contributors in in-game credits
